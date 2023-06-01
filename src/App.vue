c162988ffed20e19f11a256e201a0ea25cb9d077
eftpmc <zackhhi@gmail.com>
2023-06-01 12:00:32 -0400

    Init

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="app">
+    <div class="content">
+      <Button imageSrc="/movie.svg"/>
+      <div class="button-spacing"></div>
+      <Button imageSrc="/book.svg"/>
+      <div class="button-spacing"></div>
+      <Button imageSrc="/japn.svg"/>
+    </div>
+  </div>
+</template>
+
+<script>
+import Button from '@/components/Button.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Button,
+  },
+};
+</script>
+
+<style>
+.app {
+  background-color: #1A120B;
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.content {
+  display: flex;
+  flex-direction: row;
+}
+
+.button-spacing {
+  width: 20px;
+}
+
+</style>
---
+++ src/components/Button.vue
@@ -0,0 +1,66 @@
+<template>
+  <button class="custom-button" @click="handleClick">
+    <div class="button-icon">
+      <img :src="imageSrc" alt="Button Icon">
+    </div>
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'Button',
+  props: {
+    imageSrc: {
+      type: String,
+      required: true,
+    },
+  },
+  methods: {
+    handleClick() {
+      // Handle button click event
+      console.log('Button clicked!');
+    },
+  },
+};
+</script>
+
+<style>
+.custom-button {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100px;
+  height: 100px;
+  background-color: #D5CEA3;
+  color: white;
+  border: none;
+  border-radius: 5%;
+  cursor: pointer;
+  transition: transform 0.3s ease-in-out;
+}
+
+.custom-button:hover {
+  background-color: #3C2A21;
+  transform: scale(1.1);
+}
+
+.button-icon {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 100%;
+}
+
+.button-icon img {
+  max-width: 100%;
+  max-height: 100%;
+  transition: fill 0.3s ease-in-out;
+  filter: invert(7%) sepia(34%) saturate(622%) hue-rotate(346deg) brightness(93%) contrast(98%);
+}
+
+.custom-button:hover .button-icon img {
+  filter: invert(96%) sepia(16%) saturate(692%) hue-rotate(337deg) brightness(88%) contrast(89%);
+}
+
+</style>

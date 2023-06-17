e855484220cc57698469d241df3f94ca4ec165f8
eftpmc <zackhhi@gmail.com>
2023-06-17 18:41:22 -0400

    books.vue update + image removal

--- src/views/Books.vue
+++ src/views/Books.vue
@@ -1,14 +1,5 @@
 <template>
-  <div class="books">
-    <div class="navbar">
-      <div class="logo">
-        <span class="logo-text">ari</span>
-      </div>
-    </div>
-    <video class="background-video" autoplay loop muted playsinline preload>
-        <source src="/videos/books.mp4" type="video/mp4">
-      </video>
-  </div>
+
 </template>
 
 <script>
@@ -22,46 +13,4 @@ export default {
 };
 </script>
 
-<style scoped>
-.books {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-}
-.background-video {
-  position: relative;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  object-fit: cover;
-  z-index: 0;
-}
-.navbar {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  padding: 20px;
-  background-color: rgba(0, 0, 0, 1);
-  width: 100%;
-  height: 40px;
-  z-index: 3;
-}
-
-.logo {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-  position: relative;
-}
-
-.logo-text {
-  font-size: 24px;
-  font-weight: bold;
-  color: white;
-  margin: 0;
-  padding: 0;
-  text-align: center;
-}
-</style>
+<style scoped></style>
--- src/views/Movies.vue
+++ src/views/Movies.vue
@@ -1,8 +1,5 @@
 <template>
-  <div>
-    <h1>Movies View</h1>
-    <!-- Add your movies content here -->
-  </div>
+
 </template>
 
 <script>

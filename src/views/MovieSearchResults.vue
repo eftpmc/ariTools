b3e2508c4a4343817bdd99862f8b85f8d9463e8b
eftpmc <zackhhi@gmail.com>
2023-06-23 10:19:45 -0400

    movie specification on search instead of watch

--- src/views/MovieSearchResults.vue
+++ src/views/MovieSearchResults.vue
@@ -34,7 +34,7 @@
       async performSearch() {
         try {
          console.log(this.query);
-          const response = await fetch(`https://arpi-api.herokuapp.com/api/movie/flixhq/search/${this.query}`);
+          const response = await fetch(`https://arpi-api.herokuapp.com/api/movie/flixhq/search/movie/${this.query}`);
           const data = await response.json();
 
           if (data.length > 0) {
@@ -47,7 +47,7 @@
       },
       async fetchMovieData(movieId) {
         try {
-          const response = await fetch(`https://arpi-api.herokuapp.com/api/movie/flixhq/watch/movie/${movieId}`);
+          const response = await fetch(`https://arpi-api.herokuapp.com/api/movie/flixhq/watch/${movieId}`);
           const data = await response.json();
   
           // Retrieve the 720p video URL from the sources data
